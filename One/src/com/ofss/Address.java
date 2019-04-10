fa781702051b844a448fdc3e3f28e8903d2fd239
Guru <gurumurthy.ramamurthy@gmail.com>
2019-04-10 10:41:43 +0530

    Added Pincode in Address.java and adjusted Test.java

--- One/src/com/ofss/Address.java
+++ One/src/com/ofss/Address.java
@@ -3,16 +3,19 @@ package com.ofss;
 public class Address {
 	int flatNo;
 	String streetName,city;
-	public Address(int flatNo, String streetName, String city) {
+	long pinCode;
+	
+	public Address(int flatNo, String streetName, String city,long pinCode) {
 		super();
 		this.flatNo = flatNo;
 		this.streetName = streetName;
 		this.city = city;
+		this.pinCode=pinCode;
 	}
 	
 	@Override
 	public String toString() {
-		String msg=this.flatNo+" "+this.streetName+" "+this.city;
+		String msg=this.flatNo+" "+this.streetName+" "+this.city+" "+this.pinCode;
 		return msg;
 	}
 
--- One/src/com/ofss/Test.java
+++ One/src/com/ofss/Test.java
@@ -3,7 +3,7 @@ package com.ofss;
 public class Test {
 
 	public static void main(String[] args) {
-		Address a=new Address(12, "MG ROAD", "Bangalore");
+		Address a=new Address(12, "MG ROAD", "Bangalore",560100);
 		Employee e=new Employee("Guru", a);
 		System.out.println(e);
 
