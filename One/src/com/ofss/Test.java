a06f61aba8559eaa4469eeb32e08f2f356c652c0
Guru <gurumurthy.ramamurthy@gmail.com>
2019-04-11 16:05:54 +0530

    commit for automatic trigger kanna

--- One/src/com/ofss/Test.java
+++ One/src/com/ofss/Test.java
@@ -3,9 +3,7 @@ package com.ofss;
 public class Test {
 
 	public static void main(String[] args) {
-		Address a=new Address(12, "MG ROAD", "Bangalore",560100);
-		Employee e=new Employee("Guru", a);
-		System.out.println(e);
+		
 		System.out.println("gurubbc1 added one comment");
 		System.out.println("gurubbc added one comment");
 		System.out.println("gurubbc added two comment");
@@ -13,6 +11,7 @@ public class Test {
 		System.out.println("gurubbc1 added third comment");
 		System.out.println("gurubbc added third comment");
 		System.out.println("gurubbc branch1 first comment");
+		System.out.println("kanna going to trigger");
 	}
 
 }
